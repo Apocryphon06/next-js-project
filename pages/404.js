75b2ac9a79fccd5fc33c36fee2d4f95f55bba58a
Apocryphon06 <hrithikchandrashekar@gmail.com>
2022-10-15 21:47:35 +0530

    custom error page for 404 errors

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NotFoundFunction() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>
+        <span style={{ color: "red" }}>404</span> Page not found
+      </h1>
+    </div>
+  );
+}
+
+export default NotFoundFunction;
