f1a6c4b8ce530e6c6c95dcf488c5d8ad33ea6b49
Apocryphon06 <hrithikchandrashekar@gmail.com>
2022-10-16 11:28:27 +0530

    persist state

--- pages/404.js
+++ pages/404.js
@@ -3,9 +3,13 @@ import React from "react";
 function NotFoundFunction() {
   return (
     <div style={{ textAlign: "center" }}>
-      <h1>
+      <h2>
         <span style={{ color: "red" }}>404</span> Page not found
-      </h1>
+      </h2>
+      <p style={{ color: "#808080", fontSize: "16px" }}>
+        The page your looking for doesn't exist or has been moved to another
+        location please check the url you have typed.
+      </p>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const index = () => {
+  const [count, setcount] = useState(0);
   return (
     <div style={{ margin: "10px" }}>
       <h1 style={{ textAlign: "left" }}>Home</h1>
+      <p>{count}</p>
+      <button
+        style={{ padding: "10px", backgroundColor: "teal" }}
+        onClick={() => setcount((prevCount) => prevCount + 1)}
+      >
+        +
+      </button>
       <li>
         <Link href="/products">Products</Link>
       </li>
