54a7f705be94ca9d07fdc4324578b6e65d339d7f
Apocryphon06 <hrithikchandrashekar@gmail.com>
2022-10-17 14:08:05 +0530

    clean up

--- context/Context.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-export const Context = createContext();
-
-export const ContextProvider = (props) => {
-  const [count, setCount] = useState(0);
-  return <Context.Provider value={count}>{props.children}</Context.Provider>;
-};
